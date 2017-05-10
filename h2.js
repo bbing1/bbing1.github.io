bb39c55c620bb73897d3b8deccd1291f74376921
bbing1 <bbing1@users.noreply.github.com>
2017-05-10 13:48:23 +0100

    Update h2.js

--- h2.js
+++ h2.js
@@ -1,55 +1,52 @@
 // external js: isotope.pkgd.js
 
-
 // init Isotope
-var $grid = $('.grid').isotope({
+var iso = new Isotope( '.grid', {
   itemSelector: '.element-item',
-  layoutMode: 'fitRows',
-  getSortData: {
-    name: '.name',
-    symbol: '.symbol',
-    number: '.number parseInt',
-    category: '[data-category]',
-    weight: function( itemElem ) {
-      var weight = $( itemElem ).find('.weight').text();
-      return parseFloat( weight.replace( /[\(\)]/g, '') );
-    }
-  }
+  layoutMode: 'fitRows'
 });
 
 // filter functions
 var filterFns = {
   // show if number is greater than 50
-  numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
+  numberGreaterThan50: function( itemElem ) {
+    var number = itemElem.querySelector('.number').textContent;
     return parseInt( number, 10 ) > 50;
   },
   // show if name ends with -ium
-  ium: function() {
-    var name = $(this).find('.name').text();
+  ium: function( itemElem ) {
+    var name = itemElem.querySelector('.name').textContent;
     return name.match( /ium$/ );
   }
 };
 
 // bind filter button click
-$('#filters').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
+var filtersElem = document.querySelector('.filters-button-group');
+filtersElem.addEventListener( 'click', function( event ) {
+  // only work with buttons
+  if ( !matchesSelector( event.target, 'button' ) ) {
+    return;
+  }
+  var filterValue = event.target.getAttribute('data-filter');
+  // use matching filter function
   filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
-
-// bind sort button click
-$('#sorts').on( 'click', 'button', function() {
-  var sortByValue = $(this).attr('data-sort-by');
-  $grid.isotope({ sortBy: sortByValue });
+  iso.arrange({ filter: filterValue });
 });
 
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
+var buttonGroups = document.querySelectorAll('.button-group');
+for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+  var buttonGroup = buttonGroups[i];
+  radioButtonGroup( buttonGroup );
+}
+
+function radioButtonGroup( buttonGroup ) {
+  buttonGroup.addEventListener( 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+    event.target.classList.add('is-checked');
   });
-});
+}
